d09963f8530e5a243f71a7af142b1e1ca720ae9c
Edu <edu@bitgenio.com>
2026-02-05 16:39:18 +0100

    Update to 8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.amgoapps.moviening"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "7.0"
+        versionCode = 8
+        versionName = "8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
